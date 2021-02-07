d4db6d34ed79bb36c977f6f942da53f84ef16b38
fedgut <fedgut@gmail.com>
2021-02-07 14:05:12 -0600

    Add omniauth config for host

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
 end
+
+OmniAuth.config.full_host = "#{ENV['protocol']}#{ENV['host']}"
