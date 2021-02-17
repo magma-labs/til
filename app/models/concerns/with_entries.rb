378edcca11e9e2635caace8202237ef488bc919c
Edwin Cruz <softr8@gmail.com>
2021-02-17 13:17:41 -0600

    Skip drafts when rendering sitemap
    It prevents from accessing pages with no content

--- app/models/concerns/with_entries.rb
+++ app/models/concerns/with_entries.rb
@@ -4,6 +4,6 @@ module WithEntries
   extend ActiveSupport::Concern
 
   included do
-    scope :with_entries, -> { joins(:posts).distinct }
+    scope :with_entries, -> { joins(:posts).where.not(posts: { published_at: nil}).distinct }
   end
 end
