7d1f008b41ea581b4f305a951a9a223ac4f14298
vicmaster <velazquezgaspar16@gmail.com>
2021-02-23 03:32:44 +0100

    Update developer factory to use magmalabs domain

--- spec/factories/developer.rb
+++ spec/factories/developer.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :developer do
     sequence :email do |n|
-      "developer#{n}@example.com"
+      "developer#{n}@magmalabs.io"
     end
 
     sequence :username do |n|
