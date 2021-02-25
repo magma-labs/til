dc94724728d23b8f47e7c4630edb5b84ed09313a
vicmaster <velazquezgaspar16@gmail.com>
2021-02-25 11:30:31 +0100

    Use it instead of specify

--- spec/helpers/post_helper_spec.rb
+++ spec/helpers/post_helper_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe PostHelper do
   describe '#tweet_link' do
     context 'when returns a link to twitter' do
-      specify 'with appropriate data' do
+      it 'with appropriate data' do
         post = FactoryBot.create(:post, slug: '1234')
 
         expected_result = "<a href=\"http://twitter.com/share\" class=\"twitter-share-button\" data-text=\"Today I learned: Web Development\" data-via=\"wearemagmalabs\" data-hashtags=\"phantomjs\" data-url=\"http://test.host/posts/1234-web-development\">Tweet</a>"
@@ -13,7 +13,7 @@ describe PostHelper do
         expect(helper.tweet_link(post)).to eq expected_result
       end
 
-      specify 'with a developer\'s twitter handle' do
+      it 'with a developer\'s twitter handle' do
         developer = FactoryBot.create(:developer, twitter_handle: 'awesomehandle')
         post = FactoryBot.create(:post, developer: developer)
 
@@ -22,7 +22,7 @@ describe PostHelper do
         expect(helper.tweet_link(post)).to include expected_result
       end
 
-      specify 'with hashtag different from channel name' do
+      it 'with hashtag different from channel name' do
         developer = FactoryBot.create(:developer)
         channel = FactoryBot.create(:channel, name: 'dashy-channel',
                                               twitter_hashtag: 'popularhashtag')
